105444d1fdb71efbe565e51ba377674a9c388538
ArthurMa <4406arthur@gmail.com>
2017-10-02 23:58:15 +0800

    define repository interface

---
+++ cmd/interface/respositories.go
@@ -0,0 +1,43 @@
+package interfaces
+
+import (
+	"github.com/4406arthur/clean-micro-article-service/pkg/entity"
+)
+
+//TODO
+type DbHandler interface {
+	GetAll() ([]*map[string]interface{}, error)
+	GetByTitle(title string) (map[string]interface{}, error)
+	Save(article map[string]interface{}) error
+}
+
+type DbRepo struct {
+	dbHandler DbHandler
+}
+
+type DbArticleRepo DbRepo
+
+func (repo *DbArticleRepo) FetchAll() ([]*entity.Article, error) {
+	articles, err := repo.dbHandler.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
+
+func (repo *DbArticleRepo) GetByTitle(title string) (*entity.Article, error) {
+	article, err := repo.dbHandler.GetByTitle(title)
+	if err != nil {
+		return nil, err
+	}
+	return &article, nil
+}
+
+func (repo *DbArticleRepo) Store(article entity.Article) error {
+	err := repo.dbHandler.Save(article)
+	if err != nil {
+		return err
+	}
+	return nil
+}
