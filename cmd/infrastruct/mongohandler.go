c9a9aecf9b508660f036e14c478026414b3da773
ArthurMa <4406arthur@gmail.com>
2017-10-03 23:58:06 +0800

    new func

--- cmd/infrastruct/mongohandler.go
+++ cmd/infrastruct/mongohandler.go
@@ -4,6 +4,16 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
+func NewSqliteHandler(connString string) *MongoHandler {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	mongoHandler := new(MongoHandler)
+	sqliteHandler.Session = session
+	return mongoHandler
+}
+
 type MongoHandler struct {
 	Session *mgo.Session
 }
